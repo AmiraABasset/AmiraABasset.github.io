df90f77219447b0ea69a9e4e2b76008bdc434ebb
Amira Abdulbasset <31709957+AmiraABasset@users.noreply.github.com>
2018-08-17 05:47:37 +0200

    Add files via upload

---
+++ tanatos/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	    anchor.addEventListener('click', function (e) {
+	        e.preventDefault();
+
+			$('#menu').fadeOut();
+			
+	        document.querySelector(this.getAttribute('href')).scrollIntoView({
+	            behavior: 'smooth'
+	        });
+	    });
+	});
+
+	$('.menu_bars').click(function(){
+		$('#menu').fadeIn();
+	});
+
+	$('#menu .close').click(function(){
+		$('#menu').fadeOut();
+	});
+
+	$('.show_more').click(function(){
+		$('.works .hidden').fadeIn();
+	});
+
+	function checkClients(){
+		var leftArrow = $('.clients_say .fa-chevron-left'),
+			rightArrow =  $('.clients_say .fa-chevron-right');
+
+	$('.client:first').hasClass('active')?leftArrow.fadeOut():leftArrow.fadeIn();
+	$('.client:last').hasClass('active')?rightArrow.fadeOut():rightArrow.fadeIn();
+
+	}
+
+	checkClients();
+
+	$('.clients_say i').click(function(){
+
+		if($(this).hasClass('fa-chevron-right')){
+
+			$('.clients_say .active').fadeOut(100, function(){
+
+				$(this).removeClass('active').next('.client').addClass('active').fadeIn();
+				
+				checkClients();
+			});
+		}
+		else{
+
+			$('.clients_say .active').fadeOut(100, function(){
+
+				$(this).removeClass('active').prev('.client').addClass('active').fadeIn();
+				
+				checkClients();
+			});			
+		}
+	});
+});
