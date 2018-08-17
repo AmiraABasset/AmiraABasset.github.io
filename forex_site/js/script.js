78cc26e556369b6af163a8547fea13e92416ce20
Amira Abdulbasset <31709957+AmiraABasset@users.noreply.github.com>
2018-08-17 06:00:56 +0200

    Add files via upload

---
+++ forex_site/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	// Init AOS (animate on scroll library )
+  AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+});
