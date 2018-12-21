d48ba336fa5a9a43138b4751bbd0ddb9c68e341f
Amira Abdulbasset <31709957+AmiraABasset@users.noreply.github.com>
2018-12-21 02:23:58 +0200

    Update script.js

--- Project/js/script.js
+++ Project/js/script.js
@@ -14,6 +14,6 @@ function select_city(){
        document.getElementById("area_list").innerHTML = this.responseText;
       }
     };
-    xhttp.open("get", url, false);
+    xhttp.open("get", url, true);
     xhttp.send();
 }
